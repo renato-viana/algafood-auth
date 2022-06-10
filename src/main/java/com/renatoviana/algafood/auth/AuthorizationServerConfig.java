f1277c9f7ba5576d00f51f65a4174dd71267f8b0
Renato Viana <renatoviana30@gmail.com>
2022-06-10 19:22:50 -0300

    22.24 - Testando o fluxo Authorization Code com PKCE com o método plain

--- src/main/java/com/renatoviana/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/renatoviana/algafood/auth/AuthorizationServerConfig.java
@@ -66,7 +66,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //        security.checkTokenAccess("isAuthenticated()");
-        security.checkTokenAccess("permitAll()");
+        security.checkTokenAccess("permitAll()")
+                .allowFormAuthenticationForClients();
     }
 
     @Override
