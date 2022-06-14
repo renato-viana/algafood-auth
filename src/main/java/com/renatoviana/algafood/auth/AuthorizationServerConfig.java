858dd6c03e5525cfab015361e22aa51b289ce1dd
Renato Viana <renatoviana30@gmail.com>
2022-06-14 15:24:54 -0300

    23.9 - Assinando o JWT com RSA SHA-256 (chave assimétrica)

--- src/main/java/com/renatoviana/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/renatoviana/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.renatoviana.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import java.util.Arrays;
 
@@ -84,8 +86,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("nZr4u7x!A%D*G-KaPdSgVkYp3s5v8y/B");
+        var jwtAccessTokenConverter = new JwtAccessTokenConverter();
+
+        var jksResource = new ClassPathResource("keystores/algafood.jks");
+        var keyStorePass = "123456";
+        var keyPairAlias = "algafood";
+
+        var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+        var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+
+        jwtAccessTokenConverter.setKeyPair(keyPair);
 
         return jwtAccessTokenConverter;
     }
